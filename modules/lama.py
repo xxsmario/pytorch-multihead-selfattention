cf3adff6319039bffe22c1f4051cd80cb9acd686
xxsmario <xxsmario@proton.me>
2022-06-16 20:21:34 +0000

    Changed email address

--- modules/lama.py
+++ modules/lama.py
@@ -11,4 +11,8 @@ class LAMA(Module):
     """
     This class implements the low rank factorization multi-head self-attention mechanism described
     in `"Low Rank Factorization for Compact Multi-Head Self-Attention"
-    <https://arxiv.org/abs
+    <https://arxiv.org/abs/1912.00835>`_ by Mehta et al., 2019.
+
+    Inputs:
+
+    - inputs: 
