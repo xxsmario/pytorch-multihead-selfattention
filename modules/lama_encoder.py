eeddafa169a999a7672a1c8a51799716bc3b3ba4
xxsmario <xxsmario@proton.me>
2022-08-02 20:18:43 +0000

    \tmodified:   main.go

--- modules/lama_encoder.py
+++ modules/lama_encoder.py
@@ -10,4 +10,4 @@ from .lama import LAMA
 class LAMAEncoder(Module):
     """
     This class implements the encoder proposed in `"Low Rank Factorization for Compact Multi-Head Self-Attention"
-    <https:
+    <https://arxiv.org/abs/1912.00835>`_ by Mehta et al., 2019. This applies t
