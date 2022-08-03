ea66d88312a3c8d234fdc8b4b16ceb3c129ec020
xxsmario <xxsmario@proton.me>
2022-08-03 18:47:08 +0000

    removed legacy files

--- modules/lama_encoder.py
+++ modules/lama_encoder.py
@@ -11,4 +11,8 @@ class LAMAEncoder(Module):
     """
     This class implements the encoder proposed in `"Low Rank Factorization for Compact Multi-Head Self-Attention"
     <https://arxiv.org/abs/1912.00835>`_ by Mehta et al., 2019. This applies the attention weights of the LAMA
-    attention mechanism to the i
+    attention mechanism to the inputs returning the structured sentence embedding.
+
+    Inputs:
+
+    - inputs: 
