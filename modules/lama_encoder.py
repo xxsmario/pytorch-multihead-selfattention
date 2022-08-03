cfc8b01a9fbfe47e9e9940f4634f8415d94e834c
xxsmario <xxsmario@proton.me>
2022-08-03 08:00:56 +0000

    Add index file

--- modules/lama_encoder.py
+++ modules/lama_encoder.py
@@ -10,4 +10,5 @@ from .lama import LAMA
 class LAMAEncoder(Module):
     """
     This class implements the encoder proposed in `"Low Rank Factorization for Compact Multi-Head Self-Attention"
-    <https://arxiv.org/abs/1912.00835>`_ by Mehta et al., 2019. This applies t
+    <https://arxiv.org/abs/1912.00835>`_ by Mehta et al., 2019. This applies the attention weights of the LAMA
+    attention mechanism to the i
