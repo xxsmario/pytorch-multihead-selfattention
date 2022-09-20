a17b5e98bee3af69d6f130928ba6aa33f89facb4
xxsmario <xxsmario@proton.me>
2022-09-20 07:19:05 +0000

    added gitattributes

--- setup.py
+++ setup.py
@@ -22,4 +22,6 @@ setuptools.setup(
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Top
