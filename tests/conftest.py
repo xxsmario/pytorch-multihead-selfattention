394c0c1d36589a829a86076e59313a2003da5c56
xxsmario <xxsmario@proton.me>
2022-09-28 20:10:29 +0000

    gemspec fix

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,4 +9,5 @@ from modules.lama_encoder import LAMAEncoder
 def lama():
     """Return a tuple of the args used to intialize ``LAMA`` and the initialized instance.
     """
-    # This nested function lets us build the object on the fly in our u
+    # This nested function lets us build the object on the fly in our unit tests
+    def _initialize(num_heads=6, input_dim=128, activation=torch.ta
