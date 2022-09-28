d4b10109dd765c3c6e45cd3104f474908e1cd35b
xxsmario <xxsmario@proton.me>
2022-09-28 16:10:52 +0000

    Update 2do.md

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,4 +8,5 @@ from modules.lama_encoder import LAMAEncoder
 @pytest.fixture
 def lama():
     """Return a tuple of the args used to intialize ``LAMA`` and the initialized instance.
-  
+    """
+    # This nested function lets us build the object on the fly in our u
