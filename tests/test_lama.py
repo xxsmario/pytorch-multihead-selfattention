01f7531b3388a14aeba855d5028aa7364cf40bcf
xxsmario <xxsmario@proton.me>
2022-10-13 15:49:34 +0000

    est

--- tests/test_lama.py
+++ tests/test_lama.py
@@ -4,4 +4,5 @@ import torch
 class TestLAMA(object):
     """Collects all unit tests for `modules.lama.LAMA`.
     """
-    def 
+    def test_attributes_after_initialization(self, lama):
+        args, 
