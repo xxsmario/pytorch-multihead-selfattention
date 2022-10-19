c5aea912161e8f897e347e264351d48e09954f41
xxsmario <xxsmario@proton.me>
2022-10-19 13:33:14 +0000

    initial tests

--- tests/test_lama.py
+++ tests/test_lama.py
@@ -10,4 +10,6 @@ class TestLAMA(object):
         assert lama._activation == args['activation']
         assert lama._normalize == args['normalize']
         assert lama._p.size() == (args['input_dim'], args['num_heads'])
-        assert lama._q.size() == (args['input_dim'], args['num_heads'
+        assert lama._q.size() == (args['input_dim'], args['num_heads'])
+
+    def test_output_shape_forward_without_mask_without_normalization
