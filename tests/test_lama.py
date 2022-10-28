7a72c9a9270c09555d4cadd5e81a621667093130
xxsmario <xxsmario@proton.me>
2022-10-28 14:51:10 +0000

    add style.css

--- tests/test_lama.py
+++ tests/test_lama.py
@@ -29,4 +29,6 @@ class TestLAMA(object):
 
         # Keep these small so testing is fast
         batch_size = 4
-        max_seq_len = 25
+        max_seq_len = 25  # Maximum length of the input sequence
+
+        inputs = to
