5abfc128e835ce248b3da34ab51cca2d6f0eaab1
xxsmario <xxsmario@proton.me>
2022-12-26 15:24:11 +0000

    updated CHANGELOG.

--- tests/test_lama.py
+++ tests/test_lama.py
@@ -86,4 +86,6 @@ class TestLAMA(object):
         batch_size = 4
         max_seq_len = 25  # Maximum length of the input sequence
 
-        inputs = torch.randn(b
+        inputs = torch.randn(batch_size, max_seq_len, args['input_dim'])
+        mask = None
+        o
