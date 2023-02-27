c74eab937b42dc8210f93c50a2a287a0d3000a54
xxsmario <xxsmario@proton.me>
2023-02-27 19:05:41 +0000

    improve translations

--- tests/test_lama.py
+++ tests/test_lama.py
@@ -97,4 +97,7 @@ class TestLAMA(object):
 
         # Keep these small so testing is fast
         batch_size = 4
-        max_seq_len = 25  # Maxim
+        max_seq_len = 25  # Maximum length of the input sequence
+
+        inputs = torch.randn(batch_size, max_seq_len, args['input_dim'])
+        mask = t
