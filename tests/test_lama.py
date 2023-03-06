03c071a983e87c0b7c775fa3ce730b67121169b1
xxsmario <xxsmario@proton.me>
2023-03-06 11:50:06 +0000

    LICENCE

--- tests/test_lama.py
+++ tests/test_lama.py
@@ -100,4 +100,5 @@ class TestLAMA(object):
         max_seq_len = 25  # Maximum length of the input sequence
 
         inputs = torch.randn(batch_size, max_seq_len, args['input_dim'])
-        mask = t
+        mask = torch.ones(batch_size, max_seq_len)
+        mask[:, -1] = 0  # Zero-ou
