303a2a01ce165a2eb96cb3063173f6743636b167
xxsmario <xxsmario@proton.me>
2023-04-01 05:48:47 +0000

    usb/misc/appledisplay: Add 24" LED Cinema display

--- tests/test_lama.py
+++ tests/test_lama.py
@@ -113,4 +113,7 @@ class TestLAMA(object):
         batch_size = 4
         max_seq_len = 25  # Maximum length of the input sequence
 
-        inputs = torch.randn(batch_size, max_seq_len, args['input_dim'
+        inputs = torch.randn(batch_size, max_seq_len, args['input_dim'])
+        output = lama(inputs)
+
+        # Attention weights should sum to
